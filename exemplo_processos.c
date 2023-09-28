ac77e69056041f9a0326481c3720660c150a3053
Raphael Ramos <raphael.opensource@gmail.com>
2023-09-28 17:12:54 -0300

    refactor: process wait null function

--- exemplo_processos.c
+++ exemplo_processos.c
@@ -9,23 +9,27 @@ int main()
 {
 	pid_t pid;
 
-	/* cria um processo filho */
+	// Cria um processo filho
 	pid = fork();
 
-	if (pid < 0) { /* houve erro! */
-		/* exibir mensagem de erro */
+	// Houve erro
+	if (pid < 0) {
 		printf("\nErro da criacao de processo filho!\n");
 		return 1;
 	}
-	else if (pid == 0) { /* processo filho */
-		/* código exclusivo para o processo filho executar */
+
+	// Processo filho
+	else if (pid == 0) {
+		// Codigo exclusivo para o processo filho executar
 		printf("\nOla! Sou o processo filho!");
 		printf("\nMeu identificador (filho) eh o numero %d", getpid());	
 		printf("\nEncerrando!\n");
 	}
-	else { /* processo pai */
-		/* pai esperará até que o processo filho termine */
-		//wait(NULL);
+
+	// Processo pai
+	else {
+		// Pai esperarra ate que o processo filho termine
+		wait(NULL);
 		printf("\nOla! Sou o processo pai!");
 		printf("\nCriei um processo filho de numero %d. Ele jah encerrou!", pid);
 		printf("\nMeu identificador (pai) eh o numero %d", getpid());	
